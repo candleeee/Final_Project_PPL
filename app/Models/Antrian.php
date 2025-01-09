bf390b92f43c789a46a637f48648f3baad099c75
Mutiaaaputri <151655960+Mutiaaaputri@users.noreply.github.com>
2025-01-09 14:48:26 +0700

    Update Antrian.php

--- app/Models/Antrian.php
+++ app/Models/Antrian.php
@@ -9,7 +9,8 @@ class Antrian extends Model
 {
     use HasFactory;
     // protected $guarded = ['id'];
-    protected $fillable = ['no_antrian', 'nama', 'alamat', 'jenis_kelamin', 'no_hp', 'no_ktp', 'tgl_lahir', 'pekerjaan', 'poli', 'is_call', 'tanggal_antrian', 'user_id'];
+    protected $fillable = ['no_antrian', 'nama', 'alamat', 'jenis_kelamin', 'no_hp', 'no_ktp', 'tgl_lahir', 'pekerjaan', 'poli', 'is_call', 'tanggal_antrian', 'user_id', 'layanan',
+    'no_bpjs',];
 
     public function user()
     {
